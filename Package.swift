ce0e4bdf4a706e3bed5d81835e371912e9588861
arehman <arehman@acrobits.cz>
2024-04-22 21:42:22 +0500

    changes made to swift package

--- Package.swift
+++ Package.swift
@@ -12,14 +12,17 @@ let package = Package(
             targets: ["Softphone", "Softphone_Swift"]),
     ],
     targets: [
+        .target(
+                name: "Softphone_Swift",
+                dependencies: ["Softphone_Swift_Wrapper"],
+                resources: [.process("PrivacyInfo.xcprivacy")]),
         .binaryTarget(
             name: "Softphone",
             url: "https://dist.acrobits.net/saas-swift-package/release/Softphone.xcframework-1.1.45.zip",
             checksum: "6f9d0454284add5b9d8254fc17a2cbf04579df9c7ff350bed1505ad980962ce7"),
         .binaryTarget(
-            name: "Softphone_Swift",
+            name: "Softphone_Swift_Wrapper",
             url: "https://dist.acrobits.net/saas-swift-package/release/Softphone_Swift.xcframework-1.1.45.zip",
             checksum: "00142366a0fe852e51c0f14b29d749b2601db57cfb476ad2d42f982205b823a7"),
-            resources: [.copy("PrivacyInfo.xcprivacy")]
         ]
 )
