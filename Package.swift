9123cc506c75132e73d4eda16839cf9fa70326f3
arehman <arehman@acrobits.cz>
2024-07-23 20:18:42 +0500

    updated package.swift file and remove softphone intents

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SoftphoneSwiftPackage-saas",
-            targets: ["Softphone", "Softphone_Swift", "SoftphoneIntents"]),
+            targets: ["Softphone", "Softphone_Swift"]),
     ],
     targets: [
         .binaryTarget(
@@ -20,9 +20,5 @@ let package = Package(
             name: "Softphone_Swift",
             url: "https://dist.acrobits.net/saas-swift-package/release/Softphone_Swift.xcframework-1.1.50.zip",
             checksum: "46218a1f0bec3c9fea3d9468e4c658c782e71b1f9850977ef1ba08e7c8cd2a7b"),
-        .binaryTarget(
-            name: "SoftphoneIntents",
-            url: "https://dist.acrobits.net/saas-swift-package/release/LibsoftphoneIntents.xcframework-1.1.50.zip",
-            checksum: "8828ace4d658445c7263225574b0d63eab5bd5d5d12ad5a273a857bd66a648fc"),
         ]
 )
